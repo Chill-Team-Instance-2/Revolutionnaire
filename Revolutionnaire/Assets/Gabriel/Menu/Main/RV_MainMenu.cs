f8caf5fea1bb161edabfd0b68451e52d558704c2
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-02-01 11:41:48 +0100

    made Main Menu

---
+++ Revolutionnaire/Assets/Gabriel/Menu/Main/RV_MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RV_MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject creditMenu;
+
+    private void Start()
+    {
+        creditMenu.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void Credit()
+    {
+        creditMenu.SetActive(!creditMenu.activeSelf);
+    }
+
+    public void CloseCredit()
+    {
+        creditMenu.SetActive(false);
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
