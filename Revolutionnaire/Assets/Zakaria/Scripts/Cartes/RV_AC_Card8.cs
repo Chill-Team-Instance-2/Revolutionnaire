e2efd636d17d7a41e6636e1634506491b2972ab6
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-02-14 15:03:20 +0100

    added post processing

--- Revolutionnaire/Assets/Zakaria/Scripts/Cartes/RV_AC_Card8.cs
+++ Revolutionnaire/Assets/Zakaria/Scripts/Cartes/RV_AC_Card8.cs
@@ -21,7 +21,6 @@ public class RV_AC_Card8 : RV_AC_Parent
             switch (cardHolder.GetPlayerFromList(cardHolder.GetListOfCard(transform)))
             {
                 case 1:
-                    ActionCom();
                     break;
                 case 2:
                     ActionInt();
@@ -39,7 +38,6 @@ public class RV_AC_Card8 : RV_AC_Parent
                     ActionMil();
                     break;
                 case 1:
-                    ActionCom();
                     break;
                 case 2:
                     break;
@@ -60,6 +58,21 @@ public class RV_AC_Card8 : RV_AC_Parent
         IsActive = true;
         turnComGivePoint = RV_GameManager.Instance.Turn + 3;
         RV_GameManager.Instance.onendturn.AddListener(CheckComTurns);
+    }
+
+    public override void OnPickUp()
+    {
+        RV_ActionCard_Holder cardHolder = RV_ActionCard_Holder.Instance;
+        switch (cardHolder.GetPlayerFromList(cardHolder.GetListOfCard(transform)))
+        {
+            case 0:
+                break;
+            case 1:
+                ActionCom();
+                break;
+            case 2:
+                break;
+        }
     }
 
     public void CheckComTurns()
